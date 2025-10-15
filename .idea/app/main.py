93299e8432b2977b6378a6b1162a7ef6f7db102d
Богдан <boganan33@gmail.com>
2025-10-15 17:59:21 +0300

    New features

---
+++ .idea/app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({"message": "Hello from your CI/CD pipeline!"})
+
+@app.route("/version")
+def version():
+    try:
+        with open("version.txt") as f:
+            version = f.read().strip()
+    except FileNotFoundError:
+        version = "unknown"
+    return jsonify({"version": version})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
